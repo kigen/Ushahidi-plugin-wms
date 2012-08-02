f90bcf8c18b7bfe2c7419ba503e89354fd2b8824
Seth Kigen <Kibet1086@hotmail.com>
2012-08-02 19:42:45 +0300

    Added Configuration description

--- config/wms.php
+++ config/wms.php
@@ -5,20 +5,26 @@
  * Config file
  */
 
-//Register all layers here...
+
+/*
+ * Define all layers to be added to ushahidi maps
+ * @title : Title of the map
+ * @name: if WMS Layer from geoserver :- It is the name of the layer 
+ * @base: True if it is a base layer
+ */
+ 
 $config['layers'] = array(
     'wms_base' => array(
         'title' => "World Base Layer",
         'name' => "topp:world",
         'base' => TRUE
+    )	
+	,
+	   'za_veg' => array(
+        'title' => "South Africa vegetation",
+        'name' => "za:za_vegetation",
+        'base' => FALSE
     )
-	
-	//,
-	 //  'natural_world' => array(
-     //   'title' => "Natural Earth",
-     //   'name' => "topp:naturalearth",
-     //   'base' => FALSE
-    //)
 	,
     'state_population' => array(
         'title' => "USA State Population",
